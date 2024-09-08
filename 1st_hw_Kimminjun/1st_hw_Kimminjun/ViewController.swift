ef0d4946d280e570175b4af77fef756111e95e88
minjun kim <minzzzun@gmail.com>
2024-09-08 15:25:33 +0900

    HW1 : 시작

---
+++ 1st_hw_Kimminjun/1st_hw_Kimminjun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1st_hw_Kimminjun
+//
+//  Created by 김민준 on 9/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
