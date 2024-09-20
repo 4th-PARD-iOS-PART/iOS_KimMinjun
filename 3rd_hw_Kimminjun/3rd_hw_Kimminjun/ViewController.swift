3a6530a0071e79815dcb1778fef9df7308c4406c
minjun kim <minzzzun@gmail.com>
2024-09-21 02:12:08 +0900

    과제 3 생성

---
+++ 3rd_hw_Kimminjun/3rd_hw_Kimminjun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3rd_hw_Kimminjun
+//
+//  Created by 김민준 on 9/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 3rd_hw_Kimminjun/3rd_hw_KimminjunTests/_rd_hw_KimminjunTests.swift
@@ -0,0 +1,17 @@
+//
+//  _rd_hw_KimminjunTests.swift
+//  3rd_hw_KimminjunTests
+//
+//  Created by 김민준 on 9/21/24.
+//
+
+import Testing
+@testable import _rd_hw_Kimminjun
+
+struct _rd_hw_KimminjunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
