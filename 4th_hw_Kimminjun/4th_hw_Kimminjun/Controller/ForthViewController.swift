56eb047ac76b6d39582fb6a02cc6d27b165b3c3d
minjun kim <minzzzun@gmail.com>
2024-09-30 01:14:43 +0900

    add: 4번째 페이지 완성

--- 4th_hw_Kimminjun/4th_hw_Kimminjun/Controller/ForthViewController.swift
+++ 4th_hw_Kimminjun/4th_hw_Kimminjun/Controller/ForthViewController.swift
@@ -4,10 +4,73 @@ import UIKit
 
 class ForthViewController : UIViewController{
     
+    let downloadImage : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "downloadbig")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let downloadMessege: UILabel = {
+        let downloadMessege = UILabel()
+
+        downloadMessege.textColor = #colorLiteral(red: 0.650980413, green: 0.650980413, blue: 0.650980413, alpha: 1)
+        downloadMessege.translatesAutoresizingMaskIntoConstraints = false
+        downloadMessege.text = "Movies and TV shows that you \n download appear here."
+        downloadMessege.textAlignment = .center
+        downloadMessege.numberOfLines = 2
+        
+        return downloadMessege
+    }()
+    
+    let downloadButton : UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.background.backgroundColor = .white
+        config.attributedTitle = AttributedString("Find Something to Download",attributes: AttributeContainer([
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15.0, weight: .semibold),
+            NSAttributedString.Key.foregroundColor: UIColor.black
+        ]))
+        config.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 26, bottom: 12, trailing: 26)
+        let downloadButton = UIButton(configuration: config)
+        downloadButton.translatesAutoresizingMaskIntoConstraints = false
+        return downloadButton
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
+        setUI()
+    }
+    
+    func setUI(){
+        view.addSubview(downloadImage)
+        view.addSubview(downloadMessege)
+        view.addSubview(downloadButton)
+        
+        
+        NSLayoutConstraint.activate([
+            
+            downloadImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 100),
+//            downloadImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            downloadImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            downloadMessege.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            downloadMessege.topAnchor.constraint(equalTo: downloadImage.bottomAnchor, constant: 32),
+            
+            downloadButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            downloadButton.topAnchor.constraint(equalTo: downloadMessege.bottomAnchor, constant: 84),
+//            downloadImage.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor ,constant: -92)
+            
+            
+        ])
     }
 }
+
+
+
+
+
+
+
 
