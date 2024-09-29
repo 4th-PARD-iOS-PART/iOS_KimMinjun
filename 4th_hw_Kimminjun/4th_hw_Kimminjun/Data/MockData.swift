45547ef5bdb13cc520f93a1f2bbec82c2b0ba634
minjun kim <minzzzun@gmail.com>
2024-09-30 00:25:21 +0900

    과제 4_ 과제 3코드 복붙 완료

---
+++ 4th_hw_Kimminjun/4th_hw_Kimminjun/Data/MockData.swift
@@ -0,0 +1,66 @@
+
+
+import UIKit
+
+struct MockData {
+    let title : String
+    let image : String
+}
+
+
+extension MockData{
+    static let modeling = [
+        [
+            MockData(title: "movie1" , image: "movie1"),
+            MockData(title: "movie2" , image: "movie2"),
+            MockData(title: "movie3" , image: "movie3"),
+            MockData(title: "movie4" , image: "movie4"),
+            MockData(title: "movie5" , image: "movie5"),
+            MockData(title: "movie6" , image: "movie6"),
+        
+        ],
+        [
+            MockData(title: "movie7" , image: "movie7"),
+            MockData(title: "movie8" , image: "movie8"),
+            MockData(title: "movie9" , image: "movie9"),
+            MockData(title: "movie10" , image: "movie10"),
+            MockData(title: "movie11" , image: "movie11"),
+            MockData(title: "movie12" , image: "movie12"),
+        ],
+        [
+            MockData(title: "movie13" , image: "movie13"),
+            MockData(title: "movie14" , image: "movie14"),
+            MockData(title: "movie15" , image: "movie15"),
+            MockData(title: "movie16" , image: "movie16"),
+            MockData(title: "movie17" , image: "movie17"),
+            MockData(title: "movie18" , image: "movie18"),
+        ],
+        [
+            MockData(title: "movie19" , image: "movie19"),
+            MockData(title: "movie20" , image: "movie20"),
+            MockData(title: "movie21" , image: "movie21"),
+            MockData(title: "movie22" , image: "movie22"),
+            MockData(title: "movie23" , image: "movie23"),
+            MockData(title: "movie24" , image: "movie24"),
+        ],
+        [
+            MockData(title: "movie25" , image: "movie25"),
+            MockData(title: "movie26" , image: "movie26"),
+            MockData(title: "movie27" , image: "movie27"),
+            MockData(title: "movie28" , image: "movie28"),
+            MockData(title: "movie29" , image: "movie29"),
+            MockData(title: "movie30" , image: "movie30"),
+        ],
+        [
+            MockData(title: "movie31" , image: "movie31"),
+            MockData(title: "movie32" , image: "movie32"),
+            MockData(title: "movie33" , image: "movie33"),
+            MockData(title: "movie34" , image: "movie34"),
+            MockData(title: "movie35" , image: "movie35"),
+            MockData(title: "movie36" , image: "movie36"),
+        ],
+    ]
+}
+
+
+
