a4ea0961e685c0eebc058247e47afd51b6902a87
minjun kim <minzzzun@gmail.com>
2024-10-06 16:58:04 +0900

    hw5 : 파일 생성

---
+++ 5th_hw_Kimminjun/5th_hw_Kimminjun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  5th_hw_Kimminjun
+//
+//  Created by 김민준 on 10/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
